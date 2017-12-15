cd5f2f1f5c9ba4cc41336a0798ff5c9441a8c2a1
Daniel Öhrn <Daniel.ohrnhasslof@student.kyh.se>
2017-12-15 16:15:34 +0100

    change alt-design-2

--- js/main.js
+++ js/main.js
@@ -1,33 +1,5 @@
 'use strict'; 
 
-// Header module 
-const headerModule = (function(){
 
-    // DOM 
-    const main = document.querySelector('main'); 
-    main.className = 'main-initial'; 
-
-    const header = document.querySelector('header');
-    header.className = 'header-initial'; 
-
-    const nav = header.querySelector('#navLinks');  
-
-    window.addEventListener('scroll', colapseHeader); 
-
-    function colapseHeader(e){
-        const NAV_POSITION = 350; 
-        console.log(window.scrollY, nav.scrollY); 
-        if(window.scrollY >= NAV_POSITION) {
-            header.classList.add('header-colapsed'); 
-            header.classList.remove('header-initial'); 
-            main.className = 'main-normal';  
-        } else {
-            header.classList.remove('header-colapsed'); 
-            header.classList.add('header-initial');  
-            main.className = 'main-initial'; 
-        }
-    }
-
-})(); 
 
     
